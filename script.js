a79806791d67d64937224e1afd6ee2baa0fbb635
amandaheinzen <amanda.heinzen@escola.pr.gov.br>
2025-10-17 10:17:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener('click, mostraPalavrasChave');
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
-const texto = document.querySelector('#entrada-de-texto').value;
-const campoResultado = document.querySelector('resultado-palavrachave');
-const palavras = texto.split("")
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-campoResultado.textContent = palavras.join(", ");
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavras = texto.split(" ");
+
+  campoResultado.textContent = palavras;
 }
